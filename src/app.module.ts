1b1cf9b7f06b82df858c7fd2b281399ed08e96d6
Elmuratovich <makhmudkuzikaev@gmail.com>
2022-04-24 17:57:23 +0600

    nestjs new project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { TrackModule } from './track/track.module';
+
+@Module({
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017'),
+        TrackModule
+    ]
+})
+
+export class AppModule {}
